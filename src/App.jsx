4b5debb47dc4a762bcbb6f4c646e57056a4e4ae7
Darlan Silva <darlansilv4@gmail.com>
2022-06-22 16:06:35 -0300

    Criando componente Card

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import Card from './components/Card';
 import Form from './components/Form';
 
 function App() {
   return (
     <div className="flex flex-col h-screen w-full items-center justify-center">
       <Form />
+      <Card />
     </div>
   );
 }
