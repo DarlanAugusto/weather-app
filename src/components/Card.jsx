1e7dd513563fa19cd50350a15268c0c59e16368f
Darlan Silva <darlansilv4@gmail.com>
2022-06-22 16:19:10 -0300

    Estrutura e estilização do componente Card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,11 +1,24 @@
 import React from 'react';
 
 function Card() {
-    return (
-        <div>
-            card
-        </div>
-    );
+  return (
+    <div className="bg-white p-6 mt-10 rounded-lg shadow-md">
+      <div className="text-center">
+        <span className="block text-lg font-bold text-slate-700">Guararapes</span>
+        <span className="text-sm font-medium text-slate-400">São Paulo, Brasil</span>
+      </div>
+      
+      <div className="font-bold text-slate-700 flex mt-4 justify-center">
+        <span className="text-8xl">35</span>
+        <span className="text-2xl mt-2">ºC</span>
+      </div>
+
+      <div className="items-center flex flex-col">
+        <img src="" alt="icone" />
+        <span>nublado</span>
+      </div>
+    </div>
+  );
 }
 
 export default Card;
